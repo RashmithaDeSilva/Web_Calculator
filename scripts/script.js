0b6fac99b522d97d36d32c939c0c46858ad42884
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-06-19 21:14:07 +0530

    Upadet UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 let num = '';
-
+let mainDisplay = document.getElementById('main-display');
+let secondDisplay = document.getElementById('second-display');
 
 // Numbers
 function onClickNum(value){
     num += value;
+    secondDisplay.innerHTML = (num === undefined) ? '0':num;
     console.log(num);
 }
 
@@ -12,5 +14,6 @@ function onClickOpe(value){
     if(value === 'del'){
         num = num.substring(0, num.length-1);
     }
+    secondDisplay.innerHTML = num;
     console.log(num);
 }
