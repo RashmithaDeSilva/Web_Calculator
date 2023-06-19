acbdf3b4cee57b1ef0b49c66a2c3f11cb07c484e
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-06-20 01:49:49 +0530

    20/06/2023 work off

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,39 @@
 let num = '';
-let operator = '';
+let operator = [];
 let mainDisplay = document.getElementById('main-display');
 let secondDisplay = document.getElementById('second-display');
 
 // Numbers
 function onClickNum(value){
-    num += (num.split('.').length == 2 && value === '.') ? '':value;
-    secondDisplay.innerHTML = num;
-    //console.log(num);
+    if(!(num.length == 0 && value === '0')) {
+        num += (num.split('.').length == 2 && value === '.') ? '':value;
+        secondDisplay.innerHTML = num;
+        //console.log(num);
+    } 
 }
 
 // Operators 
 function onClickOpe(value){
+
     if(value === 'del'){
+        if(operator.length != 0 && num.endsWith(operator)) {
+            operator.pop();
+        }
         num = num.substring(0, num.length-1);
 
     } else if(value === 'c') {
         num = '';
+        operator = [];
         mainDisplay.innerHTML = (num === '') ? '0':num;
         
     } else if (value === '=') {
         
 
     } else {
-        operator = value;
+        num += value;
+        operator.push(value);
     }
+
     secondDisplay.innerHTML = (num === '') ? '0':num;
     //console.log(num);
 }
