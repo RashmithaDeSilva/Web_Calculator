7d676d488a62fcded5453fc5bc584674280a978f
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-06-20 00:19:50 +0530

    Upadet display with js operators

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,30 @@
 let num = '';
+let operator = '';
 let mainDisplay = document.getElementById('main-display');
 let secondDisplay = document.getElementById('second-display');
 
 // Numbers
 function onClickNum(value){
-    num += value;
-    secondDisplay.innerHTML = (num === undefined) ? '0':num;
-    console.log(num);
+    num += (num.split('.').length == 2 && value === '.') ? '':value;
+    secondDisplay.innerHTML = num;
+    //console.log(num);
 }
 
 // Operators 
 function onClickOpe(value){
     if(value === 'del'){
         num = num.substring(0, num.length-1);
+
+    } else if(value === 'c') {
+        num = '';
+        mainDisplay.innerHTML = (num === '') ? '0':num;
+        
+    } else if (value === '=') {
+        
+
+    } else {
+        operator = value;
     }
-    secondDisplay.innerHTML = num;
-    console.log(num);
+    secondDisplay.innerHTML = (num === '') ? '0':num;
+    //console.log(num);
 }
