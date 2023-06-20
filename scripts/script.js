2f47d877b0afbb7434acc44e2a6b88646a97c54a
Rashmitha De Silva <lahirurashmitha00@gmail.com>
2023-06-21 01:25:10 +0530

    complete JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 let num = '';
-let operator = [];
+let operator = '';
 let mainDisplay = document.getElementById('main-display');
 let secondDisplay = document.getElementById('second-display');
 
 // Numbers
 function onClickNum(value){
     if(!(num.length == 0 && value === '0')) {
-        num += (num.split('.').length == 2 && value === '.') ? '':value;
+        num += (num.split('.').length == 2 && value === '.') 
+        ? '':value;
         secondDisplay.innerHTML = num;
         //console.log(num);
     } 
@@ -16,22 +17,54 @@ function onClickNum(value){
 function onClickOpe(value){
 
     if(value === 'del'){
-        if(operator.length != 0 && num.endsWith(operator)) {
-            operator.pop();
+        if(!isNaN(num.charAt(num.length-1))) {
+            operator = '';  
         }
         num = num.substring(0, num.length-1);
 
     } else if(value === 'c') {
         num = '';
-        operator = [];
+        operator = '';
         mainDisplay.innerHTML = (num === '') ? '0':num;
         
     } else if (value === '=') {
+        if(operator === '%') {
+            let temp = num.split('%');
+            mainDisplay.innerHTML = (Number(temp[0]) / Number(temp[1]));
+
+        } else if(operator === 'x') {
+            let temp = num.split('x');
+            mainDisplay.innerHTML = (Number(temp[0]) * Number(temp[1]));
+
+        } else if(operator === '+') {
+            let temp = num.split('+');
+            mainDisplay.innerHTML = (Number(temp[0]) + Number(temp[1]));
+
+        } else if(operator === '-') {
+            let temp = num.split('-');
+            mainDisplay.innerHTML = (Number(temp[0]) - Number(temp[1]));
+        } 
         
 
     } else {
-        num += value;
-        operator.push(value);
+        if(!isNaN(num.charAt(num.length-1)) && value === '-') {
+            if(num.length !== 0 && !isNaN(num)) {
+                num += value;
+                operator = '-';
+            
+            } else if(operator.length === 0) {
+                num += value;
+            }
+        
+        } else if(operator.length === 0){
+            num += value;
+            operator = value;
+
+        } else if(operator.length !== 0 && isNaN(num.charAt(num.length-1))) {
+            num = num.substring(0, num.length-1);
+            num += value;
+            operator = value;
+        }
     }
 
     secondDisplay.innerHTML = (num === '') ? '0':num;
